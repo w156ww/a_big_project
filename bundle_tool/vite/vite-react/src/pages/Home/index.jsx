c994a4b38456dda2cae461af20e3ef5479cf04e0
wangchao <wangc5@kingnet.com>
2021-04-09 14:14:05 +0800

    add

--- bundle_tool/vite/vite-react/src/pages/Home/index.jsx
+++ bundle_tool/vite/vite-react/src/pages/Home/index.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Toolbar from "../Toolbar";
+import "./index.scss";
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <h2>home</h2>
             <Toolbar></Toolbar>
+            <div className="yes">
+                yes
+                <p>yes</p>
+                <p>yes</p>
+            </div>
+            <div className="no">
+                no
+                <p>no</p>
+                <p>no</p>
+            </div>
+            <section className="yes">
+                <p>yes</p>
+                <p>yes</p>
+            </section>
         </div>
     );
 };
